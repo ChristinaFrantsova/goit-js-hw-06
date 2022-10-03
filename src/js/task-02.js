fb3ed89f5894c005ebf827db48f45588cc852292
ChristinaFrantsova <mob0964442737@gmail.com>
2022-10-03 20:02:33 +0300

    did second task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const foodIngredients = document.querySelector("#ingredients");
+console.log(foodIngredients);
+
+const ulList = ingredients.map((element) => {
+  const foodItem = document.createElement("li");
+  foodItem.classList.add("list-item");
+  foodItem.textContent = element;
+  return foodItem;
+});
+console.log(ulList);
+
+foodIngredients.append(...ulList);
