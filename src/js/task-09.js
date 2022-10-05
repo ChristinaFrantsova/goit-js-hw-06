6da46e547d2e129fc6d6708abd399250a4b65591
ChristinaFrantsova <mob0964442737@gmail.com>
2022-10-05 15:52:49 +0300

    update task-9.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const spun = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", onButtonColorChange);
+
+function onButtonColorChange(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  spun.textContent = getRandomHexColor();
+}
