355823f65bfa4ef856ca3b295b151f6d0685a7be
ChristinaFrantsova <mob0964442737@gmail.com>
2022-10-05 15:25:35 +0300

    update task-8.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -3,13 +3,17 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
+  // console.log(event);
   event.preventDefault();
-  if (event.target.email.value === "" || event.target.password.value === "") {
+  if (
+    event.target.elements.email.value === "" ||
+    event.target.elements.password.value === ""
+  ) {
     return alert("Дорогий користувач, заповни будь-ласка всі поля !");
   } else {
-    let userDataObject = {
-      Email: event.target.email.value,
-      Password: event.target.password.value,
+    const userDataObject = {
+      Email: event.target.elements.email.value,
+      Password: event.target.elements.password.value,
     };
     console.log(userDataObject);
     event.target.reset();
