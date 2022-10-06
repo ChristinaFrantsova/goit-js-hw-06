bbd33454a9fa340b170c816dba394388d54a99de
ChristinaFrantsova <mob0964442737@gmail.com>
2022-10-06 09:53:59 +0300

    виправила

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -6,11 +6,23 @@ const categoryOfTitles = document.querySelectorAll("h2");
 const numberOfCategoryElements = document.querySelectorAll("li > ul");
 // console.log(numberOfCategoryElements);
 
-console.log("Category:", categoryOfTitles[0].textContent);
-console.log("Elements:", numberOfCategoryElements[0].children.length);
+for (let element of numberOfCategiries) {
+  console.log("Category:", element.firstElementChild.textContent);
+  console.log("Elements:", element.lastElementChild.children.length);
+}
 
-console.log("Category:", categoryOfTitles[1].textContent);
-console.log("Elements:", numberOfCategoryElements[1].children.length);
+// -------спосіб №1 (костиль)-------
+// console.log("Category:", categoryOfTitles[0].textContent);
+// console.log("Elements:", numberOfCategoryElements[0].children.length);
 
-console.log("Category:", categoryOfTitles[2].textContent);
-console.log("Elements:", numberOfCategoryElements[2].children.length);
+// console.log("Category:", categoryOfTitles[1].textContent);
+// console.log("Elements:", numberOfCategoryElements[1].children.length);
+
+// console.log("Category:", categoryOfTitles[2].textContent);
+// console.log("Elements:", numberOfCategoryElements[2].children.length);
+
+// -------спосіб №2--------
+// numberOfCategiries.forEach((element) => {
+//   console.log("Category:", element.firstElementChild.textContent);
+//   console.log("Elements:", element.lastElementChild.children.length);
+// });
